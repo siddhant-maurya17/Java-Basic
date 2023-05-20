7c5f0b0e2e4c43708847039710ae6256280f7b61
Siddhant Maurya <siddhant.maurya.1122@gmail.com>
2023-05-20 13:30:46 +0530

    Java Basic Question

---
+++ Name.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Name {
+    public static void main(String args[]){
+        try (Scanner sc = new Scanner(System.in)) {
+            
+           
+            System.out.print("Enter First No: ");
+            int a=sc.nextInt();
+            System.out.print("Enter Second No: ");
+            int b=sc.nextInt();
+
+            System.out.print("Sum of two number is:");
+            System.out.print(a+b);
+        }
+        
+    }
+    
+}
