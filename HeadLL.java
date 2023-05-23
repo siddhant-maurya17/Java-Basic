68b95da5c6e2bbb901cb10339a68677e540b5b0e
Siddhant Maurya <siddhant.maurya.1122@gmail.com>
2023-05-23 13:12:54 +0530

    LL at Head

---
+++ HeadLL.java
@@ -0,0 +1,41 @@
+import java.util.*;
+class Node{
+    int data;
+    Node next;
+    
+    Node(int data){
+        this.data=data;
+        this.next=null;
+    }
+}
+public class HeadLL
+{
+    static Node InsertAtHead(Node head,int data){
+        Node newNode=new Node(data);
+        newNode.next=head;
+        return newNode;
+    }
+    static void printNode(Node head){
+        if(head==null){
+            return;
+        }
+        while(head!=null){
+            System.out.print(head.data+" ");
+            head=head.next;
+        }
+    }
+    
+	public static void main(String[] args) {
+		try (Scanner sc = new Scanner(System.in)) {
+            int n=sc.nextInt();
+            Node head=null;
+            for (int i=0;i<n;i++) 
+             {
+                int data=sc.nextInt();
+                head=InsertAtHead(head,data); 
+             }
+            printNode(head);
+        }
+	}
+}
+   
