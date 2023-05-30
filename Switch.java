2decacc88f0ff63786a5ca6eaa3252d4446eb2b8
Siddhant Maurya <siddhant.maurya.1122@gmail.com>
2023-05-30 23:14:17 +0530

    Switch Operation

---
+++ Switch.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Switch {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter two number: ");
+            int a=sc.nextInt();
+            int b=sc.nextInt();
+            System.out.println("Enter the operation: ");
+            char ch=sc.next().charAt(0);
+            switch (ch) {
+                case '+':
+                    System.out.println(a+b);
+                    break;
+                case '-':
+                    System.out.println(a-b);
+                    break;
+                case '*':
+                    System.out.println(a*b);
+                    break;
+                case '/':
+                    System.out.println(a/b);
+                    break;
+                default:
+                    System.out.println("Expression must be +,-,*,/ or both numbers are integer");
+                    break;
+            }
+        }
+    }
+}
