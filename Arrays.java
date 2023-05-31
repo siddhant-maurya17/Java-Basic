bc84334b0ac43f39e05e50675728fc089c1e6e68
Siddhant Maurya <siddhant.maurya.1122@gmail.com>
2023-05-31 23:10:03 +0530

    Array Sum

---
+++ Arrays.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Arrays {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            int n=sc.nextInt();
+            int arr[]=new int[n];
+            for(int i=0;i<n;i++){
+                arr[i]=sc.nextInt();
+            }
+            int sum=0;
+            for(int i=0;i<n;i++){
+                sum+=arr[i];
+            }
+            System.out.println(sum);
+        }
+    }
+}
