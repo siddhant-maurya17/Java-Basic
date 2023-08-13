6c9d1886ada383429afc23bb951e340c5cc4c614
Siddhant Maurya <siddhant.maurya.1122@gmail.com>
2023-08-13 23:30:45 +0530

    Reverse Number

---
+++ Palindrome.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        // 12345 => 54321
+
+        int n=sc.nextInt();
+        int sum=0;
+        while(n>0){
+            int digit=n%10;
+            sum=(sum*10)+digit;
+            n/=10;
+        }
+        System.out.println(sum);
+        sc.close();
+    }
+}
