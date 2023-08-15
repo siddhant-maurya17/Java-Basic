787ec6d1cd194605f031862f7c3c6a7bd83ab77c
Siddhant Maurya <siddhant.maurya.1122@gmail.com>
2023-08-15 20:38:43 +0530

    Super_Reduced_String

---
+++ Mountblue Test/SuperReducedString/SuperReduced.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'superReducedString' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static String superReducedString(String s) {
+    // Write your code here
+      Stack<Character> st = new Stack<>();
+        
+        for (char c : s.toCharArray()) {
+            if (!st.isEmpty() && st.peek() == c) {
+                st.pop();
+            } else {
+                st.push(c);
+            }
+        }
+        
+        if (st.isEmpty()) {
+            return "Empty String";
+        } else {
+            StringBuilder result = new StringBuilder();
+            for (char c : st) {
+                result.append(c);
+            }
+            return result.toString();
+        }
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = bufferedReader.readLine();
+
+        String result = Result.superReducedString(s);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
