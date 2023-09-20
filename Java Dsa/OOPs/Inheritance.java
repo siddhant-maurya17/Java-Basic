d16c416e29c18e52ccf3b438b75fb4225e25bd8d
Siddhant Maurya <siddhant.maurya.1122@gmail.com>
2023-09-20 22:34:39 +0530

    Inheritance and Method Overriding

---
+++ Java Dsa/OOPs/Inheritance.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Parent{
+    int sum(int a,int b){
+    return a+b;
+    }
+}
+class Child extends Parent{
+     int sum(int a,int b){
+      return(a-b);
+    }
+}
+
+public class Inheritance{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        Parent obj1=new Parent();
+        Child obj2=new Child();
+        int result1=obj1.sum(5, 0);
+        int result2=obj2.sum(4,7);
+        System.out.print(result1+" "+result2);
+        
+        sc.close();
+    }
+}
